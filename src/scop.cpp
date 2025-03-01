b21fe8a0521bd9e2b7a235b76e431207a2b4bfe3
Ouzzhamza <houazzan@student.1337.ma>
2025-03-01 23:06:55 +0000

    Add .gitignore and implement OpenGL context setup in scop.cpp

--- src/scop.cpp
+++ src/scop.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
+#include "includes/glad/glad.h"
 #include <GLFW/glfw3.h>
-
 
 
 int main () {
@@ -16,5 +16,18 @@ int main () {
         glfwTerminate();
         return(-1);
     }
+    glfwMakeContextCurrent(window);
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Error loading glad" << std::endl;
+        return(-1);
+    }
+    glViewport(0, 0, 800, 800);
+    while(!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
